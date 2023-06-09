a35db5b62beab72ebae4a2384e9222458a83c03b
joeyweii <joey.cywei@gmail.com>
2023-06-09 13:27:13 +0800

    ✅ add test for miter construction function
    Since we add miter construction option, we also add the test for
    the option in this commit.
    In test.py,
    eq = 0 means non-equivalent cases.
    eq = 1 means equivalent cases.
    approach = 0 means construct miter approach.
    approach = 1 means construct functionality approach.

--- test/test.py
+++ test/test.py
@@ -7,37 +7,49 @@ if __name__ == '__main__':
     nQubit = 5
     nGate = 100
 
-    #  print("Start checking the equivalent cases.")
-    for seed in tqdm(range(100), desc='Checking the equivalent cases...'):
-        genUCommand = "python3 ./genRandom.py {} {} U.qasm {}".format(nQubit, nGate, seed)
-        subprocess.run(genUCommand, shell=True)
-        genVCommand = "python3 ./replaceCCX.py U.qasm V.qasm"
-        subprocess.run(genVCommand, shell=True)
+    for approach in range(2):
+        approachName = ""
+        if(approach == 0):
+            approachName = "construct miter"
+        elif(approach == 1):
+            approachName = "construct functionality"
+        else:
+            assert(0)
 
-        checkCommand = "../SliQEC_v2 --circuit1 U.qasm --circuit2 V.qasm"
-        checkOutput = subprocess.getoutput(checkCommand)
-        checkOutputJSON = json.loads(checkOutput)
-        if(checkOutputJSON["equivalence"] == "not_equivalent"):
-            print("Test failed.")
-            exit()
+        for eq in range(2):
+            eqName = ""
+            if(eq == 0):
+                eqName = "NEQ cases"
+            elif(eq == 1):
+                eqName = "EQ cases"
+            else:
+                assert(0)
 
-        subprocess.run("rm U.qasm V.qasm", shell=True)
+            for seed in tqdm(range(100), desc='{} | {}'.format(eqName, approachName)):
+                genUCommand = "python3 ./genRandom.py {} {} U.qasm {}".format(nQubit, nGate, seed)
+                subprocess.run(genUCommand, shell=True)
+                if(eq == 1):
+                    genVCommand = "python3 ./replaceCCX.py U.qasm V.qasm"
+                    subprocess.run(genVCommand, shell=True)
+                elif(eq == 0):
+                    genVtemCommand = "python3 ./replaceCCX.py U.qasm V_tem.qasm"
+                    subprocess.run(genVtemCommand, shell=True)
+                    genVCommand = "python3 ./removeGates.py V_tem.qasm V.qasm 5 {}".format(seed)
+                    subprocess.run(genVCommand, shell=True)
+                    subprocess.run("rm V_tem.qasm", shell=True)
 
-    for seed in tqdm(range(100), desc='Checking the non-equivalent cases...'):
-        genUCommand = "python3 ./genRandom.py {} {} U.qasm {}".format(nQubit, nGate, seed)
-        subprocess.run(genUCommand, shell=True)
-        genVtemCommand = "python3 ./replaceCCX.py U.qasm V_tem.qasm"
-        subprocess.run(genVtemCommand, shell=True)
-        genVCommand = "python3 ./removeGates.py V_tem.qasm V.qasm 5 {}".format(seed)
-        subprocess.run(genVCommand, shell=True)
+                checkCommand = "../SliQEC_v2 --circuit1 U.qasm --circuit2 V.qasm --approach {}".format(approach)
+                checkOutput = subprocess.getoutput(checkCommand)
+                checkOutputJSON = json.loads(checkOutput)
+                if(eq == 1):
+                    if(checkOutputJSON["equivalence"] == "not_equivalent"):
+                        print("Test failed.")
+                        exit()
+                elif(eq == 0):
+                    if(checkOutputJSON["equivalence"] == "equivalent"):
+                        print("Test failed.")
+                        exit()
 
-        checkCommand = "../SliQEC_v2 --circuit1 U.qasm --circuit2 V.qasm"
-        checkOutput = subprocess.getoutput(checkCommand)
-        checkOutputJSON = json.loads(checkOutput)
-        if(checkOutputJSON["equivalence"] == "equivalent"):
-            print("Test failed.")
-            exit()
-
-        subprocess.run("rm U.qasm V.qasm V_tem.qasm", shell=True)
+                subprocess.run("rm U.qasm V.qasm", shell=True)
 
     print("Test passed.")
